f810a6bbbab49411043648687eee1aecc77164df
mahbubpatwari <mahbubpatwari20@gmail.com>
2025-02-08 11:52:20 +0600

    version 1

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'Container1.dart';
+
+void main(){
+  runApp(Myapp());
+}
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(title: const Text("Mahbub"),),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children:[
+              ElevatedButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) =>const Container1()));
+              },
+                  child: const Text("Go To next Page"))
+            ],
+          ),
+
+
+        )
+    );
+  }
+
+}
